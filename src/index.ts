52c77886c5b20e5eded1f26d092aea0f74bd90c4
MrinalVarshney <varshneymrinal2005@gmail.com>
2024-12-19 01:40:23 +0000

    init_server

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import express, { Request, Response, NextFunction } from 'express';
+
+const PORT = 5000;
+
+const app = express();
+
+//MiddleWare
+app.use(express.json())
+
+//Routes
+app.get("/",(req:Request,res:Response)=>{
+	res.status(200).json({message:"HII from server"});
+})
+
+// Error Handling Middleware
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+     console.error(err.stack);
+         res.status(500).json({ error: 'Internal Server Error' });
+});
+
+//Start server
+app.listen(PORT,()=>{
+      console.log(`Server running on http://localhost:${PORT}`)
+})
+
+
+
+
